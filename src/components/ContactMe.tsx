62be7c7fe8a28adeca6653b0ffca93526d345a22
ThamanGurung01 <tgurung519@gmail.com>
2025-08-03 14:29:47 +0545

    copy to clipboard comp

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import CopyToClipboard from './CopyToClipBoard'
 
 const ContactMe:React.FC = () => {
   return (
     <section id='contact' className='h-screen w-full py-20 text-white'>
       <h1 className='section-heading text-4xl'>Get In Touch</h1>
-      <div className='w-1/2 mx-auto text-center'>
-      <p className='text-center text-xl'>Have a question or want to work together? Feel free to reach out using the form below.</p>
+      <div className='w-1/2 mx-auto text-center flex flex-col items-center'>
+      <p className='text-center text-xl'>Have a question or want to work together?</p>
       <div>
-        
+        <CopyToClipboard/>
       </div>
       </div>
     </section>
---
+++ src/components/CopyToClipboard.tsx
@@ -0,0 +1,34 @@
+import React, { useRef, useState } from 'react';
+import {Mail, Copy, Check } from 'lucide-react';
+import {motion} from 'framer-motion';
+const CopyToClipboard: React.FC = () => {
+  const [copied, setCopied] = useState(false);
+  const textToCopy = 'tgurung519@gmail.com';
+  const timeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+  const handleCopy = async () => {
+    try {  
+      await navigator.clipboard.writeText(textToCopy);
+      setCopied(true);
+      
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+
+      timeoutRef.current = setTimeout(() => {
+        setCopied(false);
+        timeoutRef.current = null;
+      }, 1500);
+    } catch (err) {
+      console.error('Copy failed:', err);
+    }
+  };
+
+  return (
+      <motion.button onClick={handleCopy} className="transition-all duration-300 copy_to_clipboard"
+      whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }} initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+        <Mail/> <span className='mx-2'>tgurung519@gmail.com</span> {copied ? <Check size={20} /> : <Copy size={20} />}
+      </motion.button>
+  );
+};
+
+export default CopyToClipboard;
