5862b93a241d7246fcbf3fcc0173b49f446eb7dc
ThamanGurung01 <tgurung519@gmail.com>
2025-08-03 08:28:44 +0545

    about me skill section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,10 +1,41 @@
+import { Monitor,Server ,GitBranch,Code, type LucideProps } from 'lucide-react';
 import React from 'react'
-
+import {motion} from 'framer-motion';
+import { FaReact, FaNodeJs, FaGitAlt } from 'react-icons/fa';
+import { SiExpress, SiNextdotjs, SiTailwindcss, SiTypescript, SiFramer, SiLaravel, SiMongodb, SiMysql, SiFirebase, SiGithub, SiPostman } from 'react-icons/si';
+import '../styles/About.css';
+type SkillCardType = {
+  icon: React.ForwardRefExoticComponent<Omit<LucideProps, "ref"> & React.RefAttributes<SVGSVGElement>>;
+  title: string;
+  tools: Array<{
+    icon: React.ReactNode;
+    name: string;
+  }>;
+};
 const About:React.FC = () => {
-  const skillCard: Array<{ title: string, tools: string[] }> = [
-            { title: "Frontend Development", tools: ["React", "Next.js", "Tailwind CSS", "TypeScript","Framer-motion","Blade"] },
-            { title: "Backend Development", tools: ["Node.js", "Express.js","Laravel","MongoDB", "MySQL", "Firebase"] },
-            { title: "Tools & Version Control", tools: ["Git", "GitHub", "postman"] },
+  const skillCard: SkillCardType[] = [
+            {icon:Monitor, title: "Frontend Development", tools: [
+        { icon: <FaReact className='tool-icons' color="#61DAFB" />, name: 'React' },
+        { icon: <SiNextdotjs className='tool-icons' color="#FFFFFF" />, name: 'Next.js' },
+        { icon: <SiTailwindcss className='tool-icons' color="#38B2AC" />, name: 'Tailwind CSS' },
+        { icon: <SiFramer className='tool-icons' color="#0055FF" />, name: 'Framer Motion' },
+        { icon: <Code color="#FF2D20" />, name: 'Blade' },
+      ], },
+            {icon:Server, title: "Backend Development", tools: [
+        { icon: <FaNodeJs className='tool-icons' color="#339933" />, name: 'Node.js' },
+        { icon: <SiExpress className='tool-icons' color="#FFFFFF" />, name: 'Express.js' },
+        { icon: <SiLaravel className='tool-icons' color="#FF2D20" />, name: 'Laravel' },
+        { icon: <SiMongodb className='tool-icons' color="#47A248" />, name: 'MongoDB' },
+        { icon: <SiMysql className='text-2xl' color="#FFFFFF" />, name: 'MySQL' },
+        { icon: <SiFirebase className='tool-icons' color="#FFCA28" />, name: 'Firebase' },
+      ], },
+            {icon:GitBranch, title: "Tools & Version Control", tools: [
+        { icon: <SiTypescript className='tool-icons' color="#3178C6" />, name: 'TypeScript' },
+        { icon: <FaGitAlt className='tool-icons' color="#F05032" />, name: 'Git' },
+        { icon: <SiGithub className='tool-icons' color="#FFFFFF" />, name: 'GitHub' },
+        { icon: <SiPostman className='tool-icons' color="#FF6C37" />, name: 'Postman' },
+        
+      ], },
           ];
   return (
     <section id='about' className='w-full h-screen py-20 backdrop-blur-md bg-gray-900/30 text-gray-200'>
@@ -24,16 +55,28 @@ const About:React.FC = () => {
         grow in a real-world environment, and contribute to meaningful projects.</p>
       </div>
           <h2 className='section-heading text-2xl mt-10'>Skills & Technologies</h2>
-      <div className='mt-10 grid grid-cols-3 gap-6 w-2/3 mx-auto'>
-          {skillCard.map((skill, idx) => (
-            <div key={idx} className='bg-gray-800 p-6 text-center rounded-2xl shadow-md hover:shadow-lg transition-all'>
-              <h3 className='text-xl font-semibold text-gray-100 mb-3'>{skill.title}</h3>
-              <ul className='text-gray-300 space-y-1 text-sm'>
+      <div className='mt-10 grid grid-cols-3 gap-10 w-2/3 mx-auto'>
+        {skillCard.map((skill, idx) => (
+            <motion.div
+              key={idx}
+              className="bg-gray-900 px-4 py-8 text-center rounded-2xl shadow-md hover:shadow-lg transition-all"
+              whileHover={{ scale: 1.05, y: -8 }}
+              transition={{ type: "spring", stiffness: 200, damping: 15 }}
+              style={{
+                willChange:'transform',
+              }}
+            >
+              <skill.icon className="w-12 h-12 mx-auto mb-4 text-gray-400 transition-colors duration-300 hover:text-white" />
+              <h3 className="text-xl font-semibold text-gray-100 mb-3">{skill.title}</h3>
+              <ul className="text-white space-y-2 text-sm text-left w-full pl-16">
                 {skill.tools.map((tool, i) => (
-                  <li key={i}>{tool}</li>
+                <li key={i} className="flex items-center space-x-2 text-lg">
+                  <span className="w-6 h-6">{tool.icon}</span>
+                  <span className='hover:text-white'>{tool.name}</span>
+                </li>
                 ))}
               </ul>
-            </div>
+            </motion.div>
           ))}
         </div>
     </section>
